5f3207e464a7720b10226385bf22ccd7091f7b1d
JoseAl05 <jparancibialinker@gmail.com>
2023-08-01 02:52:31 -0400

    created game details endpoint

--- src/game-details/services/game-details.service.ts
+++ src/game-details/services/game-details.service.ts
@@ -1,5 +1,6 @@
 import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
+import { catchError } from 'rxjs';
 
 @Injectable()
 export class GameDetailsService {
@@ -10,8 +11,18 @@ export class GameDetailsService {
       const url = new URL(
         `https://api.rawg.io/api/games/${gameId}?key=${process.env.RAWG_API_KEY}`,
       );
-      console.log(url);
-      return;
+      const response = await this.httpService.axiosRef
+        .get<any>(url.toString())
+        .then((res) => {
+          catchError((err) => {
+            throw new Error(err.message);
+          });
+          if (res.status === 200) {
+            return res.data;
+          }
+          throw new Error(`Error: ${res.statusText}`);
+        });
+      return response;
     }
   }
 }
