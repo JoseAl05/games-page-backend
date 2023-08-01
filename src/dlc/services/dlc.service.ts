2073c4f51ca2f78732c68b454d0bc10c3184d468
JoseAl05 <jparancibialinker@gmail.com>
2023-08-01 02:52:21 -0400

    created dlc endpoint

--- src/dlc/services/dlc.service.ts
+++ src/dlc/services/dlc.service.ts
@@ -11,8 +11,18 @@ export class DlcService {
       const url = new URL(
         `https://api.rawg.io/api/games/${gameId}/additions?key=${process.env.RAWG_API_KEY}`,
       );
-      console.log(url);
-      return;
+      const response = await this.httpService.axiosRef
+        .get<any>(url.toString())
+        .then((res) => {
+          catchError((err) => {
+            throw new Error(err.message);
+          });
+          if (res.status === 200) {
+            return res.data;
+          }
+          throw new Error(`Error: ${res.statusText}`);
+        });
+      return response;
     }
   }
 }
